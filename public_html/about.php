5faf15029236dc38562d0fe593e5482faea19fea
Allison Frideres <allison.frideres@my.simpson.edu>
2016-11-01 12:50:16 -0500

    Modified to use php include statements for the nav and footer

---
+++ public_html/about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Indianola Exotic Animals - About</title>
+           
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/sandstone/bootstrap.min.css">
+        <link rel="stylesheet" href="css/custom.css">
+    </head>
+
+    <body>
+<?php include '../resources/navigation.php'; ?>
+
+        <div class="container">
+            <div class="below_menu_container">
+                <h1><img style="width:50px;" src="img/logo.jpg" alt="website_logo"> Indianola Exotic Animals</h1>
+                
+                <h2>About Us</h2>
+        
+                <p>We are an exotic animal pet store, looking to not only sell exotic animals, but also to inform a pet lover how to take great care of their animal.</p>
+
+                <p>If you have any questions feel free to email us at: ie@test.com.</p>
+                <p>Or you can post a comment below. We'd love to hear from you!</p>
+
+                <h3>Send us a Comment!</h3>
+
+                <div class="comment_box">
+                <textarea class="form-control" rows="7" placeholder="Enter text here."></textarea>
+                <br>
+                <input class="btn btn-block btn-colored" type="submit" value="Submit">
+                </div>
+
+            </div>
+        </div>
+        
+        <?php include '../resources/footer.php'; ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ resources/navigation.php
@@ -0,0 +1,28 @@
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="hours.php">Hours and Directions</a></li>
+                        <li><a href="rightforme.php">Picking a Friend</a></li>
+                        <li><a href="forsale.php">Animals For Sale</a></li>
+                        <li><a href="info.php">Pet Care</a></li>
+                        <li><a href="petofmonth.php">Pet of the Month</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="signup.php">Sign Up</a></li>
+                        <li><a href="login.php">Log In</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
